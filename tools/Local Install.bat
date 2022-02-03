0e6523aca99b44c3177fe3258dcf73512ec2bb80
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-02-03 12:32:16 -0600

    Rework the local installer

--- tools/Local Install.bat
+++ tools/Local Install.bat
@@ -1,5 +1,5 @@
 cd ../src/PluginMerge
 
-dotnet build
-dotnet pack
-dotnet tool install --global --add-source ./bin/nupkg MJSU.Plugin.Merge
+dotnet tool uninstall -g MJSU.Plugin.Merge
+dotnet build --configuration Release /p:Version=1.0.0
+dotnet tool install --global --add-source ./bin/nupkg MJSU.Plugin.Merge --version 1.0.0
